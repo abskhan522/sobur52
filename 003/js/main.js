1d84839657d149403233f396c284fc73b95d4e45
abskhan522 <abskhan522@gmail.com>
2017-08-11 13:18:02 -0700

    003

---
+++ 003/js/main.js
@@ -0,0 +1,21 @@
+// Class 
+(function ($) {
+	"use strict";
+
+    $(document).ready(function($){
+
+        $(".video_play_btn").magnificPopup({
+            type: 'video',
+        });
+        
+        
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));
